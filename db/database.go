8baefda49e1ea14a96197118b7ca8fe4436cc1a8
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2019-05-26 20:23:06 -0300

    Add database connection and faker

--- db/database.go
+++ db/database.go
@@ -1,107 +1,59 @@
 package db
 
 import (
-	"errors"
+	"database/sql"
 	"fmt"
-	"time"
-	"workshop/config"
 	"workshop/models"
 
+	_ "github.com/lib/pq"
 	"syreclabs.com/go/faker"
 )
 
-var localdb map[string]interface{}
-var users = map[int]models.User{}
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "niconisoria"
+	password = ""
+	dbname   = ""
+)
 
-type DBPurchases struct{}
+var db = initDB()
+var DBUsers = Users{}
+var DBPurchases = Purchases{}
 
-func (dbp *DBPurchases) Save(key string, value models.Purchase) error {
-	if _, ok := localdb[key]; ok {
-		return errors.New(fmt.Sprintf("Purchase whit key %v already exist.", key))
+func initDB() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
 	}
-	localdb[key] = value
-	return nil
-}
 
-func (dbp *DBPurchases) GetAll() map[string]interface{} {
-	return localdb
-}
-
-func (dbp *DBPurchases) GetById(key string) (interface{}, error) {
-	if _, ok := localdb[key]; ok {
-		return localdb[key], nil
-	} else {
-		return nil, errors.New(fmt.Sprintf("Purchase with key %v not found ", key))
-	}
-}
-
-func (dbp *DBPurchases) Update(key string, purchase models.Purchase) (interface{}, error) {
-	if _, ok := localdb[key]; ok {
-		localdb[key] = purchase
-		return localdb[key], nil
-	} else {
-		return nil, errors.New(fmt.Sprintf("Purchase with key %v not found ", key))
-	}
-}
-
-func (dbp *DBPurchases) Delete(key string) string {
-	var message = " purchase " + key + " doesn't exist"
-	if _, ok := localdb[key]; ok {
-		resultado := models.Purchase{}
-		resultado = localdb[key].(models.Purchase)
-		if resultado.Status == config.FINISHED {
-			return fmt.Sprintf("Purchase id:" + key + " has final status, cant be deleted ")
-		}
-		delete(localdb, key)
-		if _, ok := localdb[key]; !ok {
-			message = fmt.Sprintf("Purchase id:" + key + " deleted ")
-		} else {
-			message = fmt.Sprintf("Purchase id:" + key + " cant be deleted ")
-		}
-	}
-	return message
+	return db
 }
 
 func init() {
-	fmt.Println("--------------------- INIT db ---------------------")
-	localdb = map[string]interface{}{}
-	users = map[int]models.User{}
 	for i := 1; i <= 3; i++ {
 		u := models.User{
 			DNI:      faker.RandomInt(20000000, 40000000),
-			ID:       int64(i),
 			Name:     faker.Name().FirstName(),
 			LastName: faker.Name().LastName(),
 		}
-		users[i] = u
+		DBUsers.Save(u)
 	}
-	layout := "2006-01-02T15:04:05.000Z"
-	str := "2018-11-12T11:45:26.371Z"
-	str2 := "2019-11-12T11:45:26.371Z"
-	t1, _ := time.Parse(layout, str)
-	t2, _ := time.Parse(layout, str2)
 
-	for i := 1; i <= 10; i++ {
-		id := fmt.Sprintf("%v", i)
+	for i := 1; i <= 3; i++ {
+		user := models.User{
+			ID: int64(i),
+		}
 		purchase := models.Purchase{
-			Amount:       faker.Commerce().Price(),
-			ID:           id,
-			Image:        fmt.Sprintf("https://loremflickr.com/320/240?random=%v", i),
-			Title:        faker.Commerce().ProductName(),
-			CreationDate: faker.Date().Between(t1, t2),
+			User:   user,
+			Amount: faker.Commerce().Price(),
+			Image:  fmt.Sprintf("https://loremflickr.com/320/240?random=%v", i),
+			Title:  faker.Commerce().ProductName(),
 		}
-		switch i % 3 {
-		case 0:
-			purchase.Status = config.FINISHED
-			purchase.User = users[1]
-		case 1:
-			purchase.Status = config.NEW
-			purchase.User = users[2]
-		case 2:
-			purchase.Status = config.CANCELLED
-			purchase.User = users[3]
-		}
-		localdb[id] = purchase
+		purchase.GenerateID()
 	}
 
 }
