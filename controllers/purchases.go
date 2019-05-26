f832e2717e10c943a4a28caf55fb27ecb6d3fd9b
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2019-05-26 20:22:28 -0300

    Modify controllers to work with database

--- controllers/purchases.go
+++ controllers/purchases.go
@@ -7,24 +7,31 @@ import (
 	"workshop/services"
 	"workshop/tools"
 
+	"github.com/gin-gonic/gin/binding"
+
 	"github.com/gin-gonic/gin"
 )
 
 func CreatePurchase(c *gin.Context) {
 	purchase := models.Purchase{}
+	user := models.User{}
 
-	if err := c.BindJSON(&purchase); err != nil {
-		c.JSON(http.StatusBadRequest, err)
+	if err := c.ShouldBindBodyWith(&purchase, binding.JSON); err != nil {
+		c.JSON(http.StatusBadRequest, fmt.Sprint(err))
 		return
 	}
 
+	if err := c.ShouldBindBodyWith(&user, binding.JSON); err != nil {
+		c.JSON(http.StatusBadRequest, fmt.Sprint(err))
+		return
+	}
 	if !purchase.IsValid() {
 		c.JSON(http.StatusBadRequest, "Invalid purchase params")
 		return
 	}
-	newPurchase, err := services.CreatePurchase(purchase)
+	newPurchase, err := services.CreatePurchase(purchase, user)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
+		c.JSON(http.StatusInternalServerError, fmt.Sprint(err))
 		return
 	}
 
@@ -60,7 +67,7 @@ func ReadPurchases(c *gin.Context) {
 	}
 
 	if purchase, err := services.GetPurchaseByID(id); err != nil {
-		c.JSON(http.StatusBadRequest, fmt.Sprint(err))
+		c.JSON(http.StatusBadRequest, fmt.Sprint("Invalid id"))
 		return
 	} else {
 		c.JSON(http.StatusOK, purchase)
@@ -75,6 +82,5 @@ func DeletePurchase(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, "invalid params")
 		return
 	}
-	c.JSON(http.StatusAccepted, fmt.Sprint(services.DeletePurchase(id)))
-	return
+	c.JSON(http.StatusOK, fmt.Sprint(services.DeletePurchase(id)))
 }
